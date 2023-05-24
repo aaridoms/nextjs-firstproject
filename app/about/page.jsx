c77dd1a09a3327273c1705d4ee1fbb6863897785
Ángel Arias <angadominguez96@gmail.com>
2023-05-24 17:45:59 +0200

    firts commit

---
+++ app/about/page.jsx
@@ -0,0 +1,15 @@
+function AboutPage() {
+  return (
+    <div>
+        <h1>About</h1>
+        <p>Ipsum nisi eu nostrud qui elit culpa ipsum duis pariatur sunt magna.
+             Aute ipsum ea est pariatur minim non nisi esse in nisi. Qui incididunt qui magna
+              proident ea ea culpa enim quis ad occaecat ullamco reprehenderit aliqua.
+               Et Lorem sunt do fugiat labore deserunt esse nulla est ipsum. Ex sunt enim
+                et voluptate ad esse eu exercitation minim eiusmod exercitation.
+        </p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ app/services/page.jsx
@@ -0,0 +1,14 @@
+function ServicesPage() {
+  return (
+    <div>
+        <h1>Services</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure recusandae laboriosam deserunt officiis
+             laudantium natus voluptates accusantium soluta? Magni, eum. Expedita eos molestiae eaque? Ab, molestias
+              culpa! Perspiciatis, quod! Quod qui at blanditiis iure accusantium similique voluptatibus harum ad doloremqu
+              e labore sint, ipsa non repellendus perspiciatis. Molestias odio ullam voluptatibus.
+        </p>
+    </div>
+  )
+}
+
+export default ServicesPage
